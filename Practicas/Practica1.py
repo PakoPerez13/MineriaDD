a52b495c38e927503449904090acadaae20c71fd
PakoPerez13 <89920305+PakoPerez13@users.noreply.github.com>
2023-09-13 14:04:27 -0600

    Update Practica1.py

--- Practicas/Practica1.py
+++ Practicas/Practica1.py
@@ -4,5 +4,3 @@ import os
 def get_csv_kaggle ():
     kaggle.api.authenticate()
     kaggle.api.dataset_download_files('https://www.kaggle.com/datasets/medaxone/fics-chess-dataset-2021')
-
-df= get_csv_kaggle()
